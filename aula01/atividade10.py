a939a0df0b964f38aa32059fd785fde6fba948a7
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-04-29 12:02:07 -0300

    Update atividade10.py

--- aula01/atividade10.py
+++ aula01/atividade10.py
@@ -1,16 +1,16 @@
-def produto(primeiro_numero, segundo_numero):
+def Produto(primeiro_numero, segundo_numero):
     return (primeiro_numero*2) * (segundo_numero/2)
 
-def soma(primeiro_numero, terceiro_numero):
+def Soma(primeiro_numero, terceiro_numero):
     return primeiro_numero*3 + terceiro_numero
 
-def potencia(terceiro_numero):
+def Potencia(terceiro_numero):
     return terceiro_numero**3
 
 primeiro_numero = float(input("Digite o primeiro número: "))
 segundo_numero = float(input("Digite o segundo número: "))
 terceiro_numero = float(input("Digite o terceiro número: "))
 
-print("O produto do dobro do primeiro com metade do segundo é: ", produto(primeiro_numero, segundo_numero))
-print("A soma do triplo do primeiro com o terceiro é: ", soma(primeiro_numero, terceiro_numero))
-print("O terceiro elevado ao cubo é: ", potencia(terceiro_numero))
+print("O produto do dobro do primeiro com metade do segundo é: ", Produto(primeiro_numero, segundo_numero))
+print("A soma do triplo do primeiro com o terceiro é: ", Soma(primeiro_numero, terceiro_numero))
+print("O terceiro elevado ao cubo é: ", Potencia(terceiro_numero))
