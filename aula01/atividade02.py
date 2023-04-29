bd12911bd3ab129b4cd0035cb04806baa64a87fb
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-04-29 10:32:32 -0300

    preview answears

---
+++ aula01/atividade02.py
@@ -0,0 +1,2 @@
+numero = int(input("Digite um número: "))
+print(f"O numero informado foi: {numero}")
---
+++ aula01/atividade03.py
@@ -0,0 +1,7 @@
+primeiro_numero = int(input("Digite o primeiro número: "))
+segundo_numero = int(input("Digite o segundo número: "))
+terceiro_numero = int(input("Digite o terceiro número: "))
+
+soma = primeiro_numero + segundo_numero + terceiro_numero
+
+print(f"A soma de {primeiro_numero}, {segundo_numero} e {terceiro_numero} é: {soma}")
